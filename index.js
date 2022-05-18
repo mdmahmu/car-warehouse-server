4fb54d507accf978cb5114d5eaa9cc5a47371a6d
mmmmasum <mmm.masum@yahoo.com>
2022-05-18 17:12:12 +0600

    dynamic route done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import 'dotenv/config';
+import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const uri = "mongodb+srv://dbusermmm:dbusermmmhdu@cluster0.pxon5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pxon5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -17,6 +18,23 @@ async function run() {
         await client.connect();
         const carsCollection = client.db("carWarehouse").collection("cars");
 
+        // GET all cars
+        app.get('/cars', async (req, res) => {
+            const query = {};
+            const cursor = carsCollection.find(query);
+            const cars = await cursor.toArray();
+            res.send(cars);
+        });
+
+        // GET single car
+        app.get('/cars/:inventoryId', async (req, res) => {
+            const id = req.params.inventoryId;
+            const query = { _id: ObjectId(id) };
+            const result = await carsCollection.findOne(query);
+            res.send(result);
+        });
+
+        // POST car
         app.post('/cars', async (req, res) => {
             const newCar = req.body;
             const result = await carsCollection.insertOne(newCar);
