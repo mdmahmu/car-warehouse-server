c4df681d755aae5d73f6325bd16537b7d91e289c
mmmmasum <mmm.masum@yahoo.com>
2022-05-28 17:50:55 +0600

    jwt implementation done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import jwt from 'jsonwebtoken';
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,15 +10,37 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+//verifying jwt
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 //database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pxon5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-
     try {
         await client.connect();
         const carsCollection = client.db("carWarehouse").collection("cars");
+
+        //auth
+        app.post('/login', async (req, res) => {
+            const email = req.body;
+            const jwtAccessToken = jwt.sign(email, process.env.ACCESS_TOKEN, { expiresIn: '30d' });
+            res.send({ jwtAccessToken });
+        });
 
         // GET all cars
         app.get('/cars', async (req, res) => {
@@ -84,18 +107,22 @@ async function run() {
         });
 
         //get my items
-        app.get('/my_items', async (req, res) => {
+        app.get('/my_items', verifyJWT, async (req, res) => {
+            const decodedEmailOrUid = req.query.emailOrUid;
             const emailOrUid = req.query.emailOrUid;
-            const query = { emailOrUid: emailOrUid };
-            const result = carsCollection.find(query);
-            const myItems = await result.toArray();
-            res.send(myItems);
+            if (emailOrUid === decodedEmailOrUid) {
+                const query = { emailOrUid: emailOrUid };
+                const result = carsCollection.find(query);
+                const myItems = await result.toArray();
+                res.send(myItems);
+            } else {
+                res.status(403).send({ message: 'forbidden access' });
+            }
         });
     }
     finally {
         // await client.close();
     }
-
 }
 run().catch(console.dir);
 
